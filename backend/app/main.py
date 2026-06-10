7e046ce3602f2f75d6833be936ec75e984d8f428
Niya <niyapsherry@gmail.com>
2026-06-10 23:53:29 +0530

    feat: configure database and user model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-
 from app.api.routes.health import router as health_router
-
+from app.db.database import Base, engine
+from app import models
 app = FastAPI(title="Mini Classroom API")
-
+Base.metadata.create_all(bind=engine)
 app.include_router(health_router)
